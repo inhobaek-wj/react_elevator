afa106a1b61d61680960760261af1cd17c48d0d0
jake <inhobaek.wj@gmail.com>
2023-02-01 18:35:50 +0900

    feat: publish mayGoToQueue

--- src/BuildingComponent.js
+++ src/BuildingComponent.js
@@ -2,6 +2,7 @@ import './App.css';
 
 function BuildingComponent() {
     const buttons = [];
+    const mayGoToQueue = [1, 2];
     const numOfTotalFloors = 20;
     const numOfElevator = 3;
     for (let i = 0; i < numOfTotalFloors; i++) {
@@ -28,16 +29,22 @@ function BuildingComponent() {
 
     return (
         <div className="pt-20 h-screen border-solid border-2 border-sky-500 flex flex-col justify-center">
-            <div className="h-2/6 mb-5 border-solid border-2 border-blue-500">
-                {buttons.map(floor => {
-                    return <div key={floor}
-                                className="ml-8 inline-block"
-                                data-testid="building-btn">
-                        {floor}
-                        {floor !== 20 && <span>u</span>}
-                        {floor !== 1 && <span>d</span>}
-                    </div>;
-                })}
+            <div className="h-2/6 mb-5 border-solid border-2 border-blue-500 flex flex-col justify-around">
+                <div>
+                    {buttons.map(floor => {
+                        return <div key={floor}
+                                    className="ml-8 inline-block"
+                                    data-testid="building-btn">
+                            {floor}
+                            {floor !== 20 && <span>u</span>}
+                            {floor !== 1 && <span>d</span>}
+                        </div>;
+                    })}
+                </div>
+
+                <div className="ml-8">
+                    <p>mayGoTo: <strong>{mayGoToQueue.join(',')}</strong></p>
+                </div>
             </div>
             <div className="h-4/6 flex justify-around">{elevators()}</div>
         </div>
